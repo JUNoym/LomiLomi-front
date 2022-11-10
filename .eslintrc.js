8972659d0c702f2915e0cf1c7b07161fe5fd466e
Junya Oyama <junya.oyama@MacBook-Pro-121.local>
2022-11-11 02:10:50 +0900

    eslint&prettier設定

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,32 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "prefer-const": "error",
+    "react/prop-types": "off",
+    "no-undef": "error",
+  },
+  settings: {
+    react: {
+      version: "detect",
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "semi": ["error", "always"],
-        "quotes": ["error", "double"],
-        "react/prop-types": "off",
-        "no-undef": "error",
-    }
+  },
 };
--- app/(beforeLogin)/top/page.tsx
+++ app/(beforeLogin)/top/page.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 export default function Home() {
-    return (
-      <div>
-        <h1>ログイン前のページ</h1>
-        <p>ここではヘッダーにログインボタンが表示される</p>
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <h1>ログイン前のページ</h1>
+      <p>ここではヘッダーにログインボタンが表示される</p>
+    </div>
+  );
+}
